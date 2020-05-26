2f1c48ba7a753958fbbc4996013e74938ab2eb86
shamable <48834168+shamable@users.noreply.github.com>
2020-05-26 23:35:13 +0200

    fin service mail et visuelle de la page

---
+++ template_mail.py
@@ -0,0 +1,9 @@
+def debut_email():
+	email ='''<html><head>
+	</head>
+	<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #222222;">'''
+	return email
+
+def end_email():
+	email = '</body></html>'
+	return email
